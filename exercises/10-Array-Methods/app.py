d38d18b09305de86de5d1155965bef0218faedec
Giousned <guille_ranza@hotmail.com>
2023-03-31 08:07:22 +0000

    [ADD] otra forma ultimo ejercicio

--- exercises/10-Array-Methods/app.py
+++ exercises/10-Array-Methods/app.py
@@ -2,7 +2,15 @@ names = ['John', 'Kenny', 'Tom', 'Bob', 'Dilan']
 ## CREATE YOUR FUNCTION HERE
 
 
-def sort_names (lista):
-    return sorted(lista)
+def sort_names(lista):
+    lista.sort()
+    return lista
 
 print(sort_names(names))
+
+
+
+# def sort_names(lista):
+#     return sorted(lista)
+
+# print(sort_names(names))
